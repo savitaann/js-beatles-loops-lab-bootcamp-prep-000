f7e2df445dceb4ca3c8874d3e27c61e6662b404d
savita madray <savitamadray01@gmail.com>
2017-10-06 19:57:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 function theBeatlesPlay(musicians, instruments){
-  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
     var phrases = [];
     for (var i = 0; i < musicians.length; i++) {
       phrases.push(musicians[i] + " plays " + instruments[i]);
@@ -9,14 +7,7 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 
-function johnLennonFacts(){
-  const facts = [
-    "foo",
-    "bar",
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
+function johnLennonFacts(facts){
 
    var newFacts = [];
    var i = 0
