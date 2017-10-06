c1451509de375751615bc821d7960bd79c07796f
savita madray <savitamadray01@gmail.com>
2017-10-06 19:44:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 
-function johnLennonFacts(a, b{
+function johnLennonFacts(a, b){
   const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
