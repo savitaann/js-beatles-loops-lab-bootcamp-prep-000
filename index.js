dfab329f4f736c3fb6276ae6950acd2679d3e1db
savita madray <savitamadray01@gmail.com>
2017-10-06 19:35:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function johnLennonFacts(){
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-  "foo"
+  "He hated the sound of his own voice",
+  "foo",
   "bar"];
 
    var newFacts = [];
